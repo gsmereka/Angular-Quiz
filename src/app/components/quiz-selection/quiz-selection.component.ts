94fcfa0021eaecbea1795f0dacd3cc8ccd8b07e0
gsmereka <gsmereka200@gmail.com>
2025-04-08 19:41:10 -0300

    feat: add quiz-selection, and update home css

---
+++ src/app/components/quiz-selection/quiz-selection.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quiz-selection',
+  imports: [],
+  templateUrl: './quiz-selection.component.html',
+  styleUrl: './quiz-selection.component.css'
+})
+export class QuizSelectionComponent {
+
+}
