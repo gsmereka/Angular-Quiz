b3fcb0fe46caa59b35145e5397add40e4d6cf15a
gsmereka <gsmereka200@gmail.com>
2025-04-09 18:09:15 -0300

    feat: add quiz page

---
+++ src/app/pages/quiz-page/quiz-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { QuizComponent } from '../../components/quiz/quiz.component';
+
+@Component({
+  selector: 'app-quiz-page',
+  imports: [QuizComponent],
+  templateUrl: './quiz-page.component.html',
+  styleUrl: './quiz-page.component.css'
+})
+export class QuizPageComponent {
+
+}
