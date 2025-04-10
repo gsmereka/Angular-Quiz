cbd883248f9b43fe58b0332b60d15bf8d69ec452
Gabriel Smereka <102737099+gsmereka@users.noreply.github.com>
2025-04-10 18:23:46 -0300

    feat: add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { QuizComponent } from './components/quiz/quiz.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent, pathMatch: 'full' },
+    { path: 'path', component: QuizComponent }
+  ];
