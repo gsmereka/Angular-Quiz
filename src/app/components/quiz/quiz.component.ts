3fbd3fae4a9f3fa4eda5d73ab019d92d48a6a86a
Gabriel Smereka <102737099+gsmereka@users.noreply.github.com>
2025-04-10 22:33:35 -0300

    fix: quiz route accepts id correctly

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -1,82 +1,81 @@
 import { Component, Input, OnInit } from '@angular/core';
-import quiz_questions from "../../../assets/data/quizz_questions.json"
 import { CommonModule } from '@angular/common';
+import { JsonLoaderService } from '../../services/json-loader.service';
 
 @Component({
   selector: 'app-quiz',
   imports: [CommonModule],
   templateUrl: './quiz.component.html',
-  styleUrl: './quiz.component.css'
+  styleUrls: ['./quiz.component.css']
 })
-
 export class QuizComponent implements OnInit {
-  @Input()
-  quiz_id: String = ""
-  title:string = ""
+  @Input() quiz_id: string = '';
 
-  questions:any
-  questionSelected:any
+  title: string = '';
+  fileUrl: string = 'assets/data/quizz_1.json';
+  questions: any;
+  questionSelected: any;
 
-  answers:string[] = []
-  answerSelected:string =""
+  answers: string[] = [];
+  answerSelected: string = '';
 
-  questionIndex:number =0
-  questionMaxIndex:number=0
+  questionIndex: number = 0;
+  questionMaxIndex: number = 0;
 
-  finished:boolean = false
-  logoPath: String = "assets/imgs/logo.png"
+  finished: boolean = false;
+  logoPath: string = 'assets/imgs/logo.png';
+  quizData: any;
 
-  constructor() { }
+  constructor(private jsonLoaderService: JsonLoaderService) {}
 
   ngOnInit(): void {
-    if(quiz_questions){
-      this.finished = false
-      this.title = quiz_questions.title
+    this.jsonLoaderService.loadJsonFile(this.fileUrl).subscribe(
+      (data) => {
+        if (data) {
+          this.quizData = data;
+          this.finished = false;
+          this.title = data.title;
 
-      this.questions = quiz_questions.questions
-      this.questionSelected = this.questions[this.questionIndex]
+          this.questions = data.questions;
+          this.questionSelected = this.questions[this.questionIndex];
 
-      this.questionIndex = 0
-      this.questionMaxIndex = this.questions.length
-
-      // console.log(this.questionIndex)
-      // console.log(this.questionMaxIndex)
-    }
-
-  }
-
-  playerChoose(value:string){
-    this.answers.push(value)
-    this.nextStep()
-
-  }
-
-  async nextStep(){
-    this.questionIndex+=1
-
-    if(this.questionMaxIndex > this.questionIndex){
-        this.questionSelected = this.questions[this.questionIndex]
-    }else{
-      const finalAnswer:string = await this.checkResult(this.answers)
-      this.finished = true
-      this.answerSelected = quiz_questions.results[finalAnswer as keyof typeof quiz_questions.results ]
-    }
-  }
-
-  async checkResult(anwsers:string[]){
-
-    const result = anwsers.reduce((previous, current, i, arr)=>{
-        if(
-          arr.filter(item => item === previous).length >
-          arr.filter(item => item === current).length
-        ){
-          return previous
-        }else{
-          return current
+          this.questionIndex = 0;
+          this.questionMaxIndex = this.questions.length;
         }
-    })
-
-    return result
+      },
+      (error) => {
+        console.error('Erro ao carregar o arquivo JSON', error);
+      }
+    );
   }
 
+  playerChoose(value: string) {
+    this.answers.push(value);
+    this.nextStep();
+  }
+
+  nextStep() {
+    this.questionIndex += 1;
+
+    if (this.questionMaxIndex > this.questionIndex) {
+      this.questionSelected = this.questions[this.questionIndex];
+    } else {
+      this.checkResult(this.answers).then(finalAnswer => {
+        this.finished = true;
+        this.answerSelected = this.quizData.results[finalAnswer as keyof typeof this.quizData.results];
+      });
+    }
+  }
+
+  async checkResult(answers: string[]) {
+    const result = answers.reduce((previous, current, i, arr) => {
+      if (arr.filter(item => item === previous).length > arr.filter(item => item === current).length) {
+        return previous;
+      } else {
+        return current;
+      }
+    });
+
+    return result;
+  }
 }
